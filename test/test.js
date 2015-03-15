ae4307beb45d4f70610b92a58d15da7b502740a1
John Betancur <johnnyazee@gmail.com>
2015-03-15 12:29:33 -0400

    updated nan

--- test/test.js
+++ test/test.js
@@ -1,5 +1,14 @@
 var assert = require('assert');
-var pam = require('../build/Release/authenticate_pam');
+var pam;
+
+if (process.platform === "linux" && process.arch === "x64") {
+	pam = require('../bin/linux_x64/authenticate_pam');
+} else if (process.platform === "darwin" && process.arch === "x64") {
+	pam = require('../bin/darwin_x64/authenticate_pam');
+} else {
+	// Load the new built binary for other platforms.
+	pam = require('../build/Release/authenticate_pam');
+}
 
 pam.authenticate(process.argv[2], process.argv[3],
 	function(err) {
