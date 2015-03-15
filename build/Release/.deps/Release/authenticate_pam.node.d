09a9857644a7c4a050564142e977ef10f7606ddb
John Betancur <johnnyazee@gmail.com>
2015-03-15 09:31:20 -0400

    Added automations to make it cross platform with darwin/linux - will auto load via index.js

--- build/Release/.deps/Release/authenticate_pam.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/authenticate_pam.node := ./gyp-mac-tool flock ./Release/linker.lock c++ -bundle -Wl,-search_paths_first -mmacosx-version-min=10.5 -arch x86_64 -L./Release  -o Release/authenticate_pam.node Release/obj.target/authenticate_pam/src/authenticate_pam.o -undefined dynamic_lookup -lpam
