3044f6edd39440401dd98d3cd245a1d5414c42d5
John Betancur <johnnyazee@gmail.com>
2015-03-15 09:25:47 -0400

    Added automations to make it cross platform with darwin/linux - will auto load via index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+var authenticate_pam = null;
+ 
+// Load the precompiled binary for windows.
+if (process.platform === "linux" && process.arch === "x64") {
+	authenticate_pam = require('./bin/linux_x64/authenticate_pam');
+} else if (process.platform === "darwin" && process.arch === "x64") {
+	authenticate_pam = require('./bin/darwn_x64/authenticate_pam');
+} else {
+	// Load the new built binary for other platforms.
+	authenticate_pam = require('./build/Release/authenticate_pam');
+}
+ 
+module.exports = authenticate_pam;
